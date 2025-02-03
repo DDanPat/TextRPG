45ffcda3a9ff4c2d830011293093d8b0c72e3710
DDanPat <hyoung6855@gmail.com>
2025-02-03 16:14:02 +0900

    게임 종료 선택지 추가
    메뉴에서 게임종료 선택지 추가

--- TextRPG/Program.cs
+++ TextRPG/Program.cs
@@ -40,13 +40,13 @@
 
     public class MainStage : Player
     {
-        Player player = new Player();
+        bool game = true;
         public void Start()
         {
             Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
             Console.WriteLine("원하시는 이름을 설정 해주세요.");
             Console.Write("이름 : ");
-            player.Name = Console.ReadLine();
+            this.Name = Console.ReadLine();
 
             //저장 시스템 만들기
 
@@ -56,7 +56,8 @@
         }
         public void MenuUI()
         {
-            while (true)
+            
+            while (game)
             {
                 Console.Clear();
                 Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
@@ -64,6 +65,7 @@
                 Console.WriteLine("1. 상태 보기");
                 Console.WriteLine("2. 인벤토리");
                 Console.WriteLine("3. 상점\n");
+                Console.WriteLine("0. 게임 종료\n");
                 Console.WriteLine("원하시는 행동을 입력해주세요.");
                 Console.Write(">> ");
                 string choice = Console.ReadLine();
@@ -79,8 +81,12 @@
                     case "3":
                         ShopUI();
                         break;
+                    case "0":
+                        game = false;
+                        return;
                     default:
-                        Console.WriteLine("잘못된 입력입니다.");
+                        Console.WriteLine("잘못된 입력입니다. 계속하려면 아무 키나 누르세요...");
+                        Console.ReadKey();
                         break;
                 }
             }
@@ -89,75 +95,109 @@
 
         public void StateUI()
         {
-            Console.Clear();
-            Console.WriteLine("상태 보기");
-            Console.WriteLine("캐릭터의 정보가 표시됩니다.\n");
-            Console.WriteLine($"Lv. {player.Level}");
-            Console.WriteLine($"{player.Name} ( {player.ClassName} )");
-            Console.WriteLine($"공격력 : {player.AttackPower}");
-            Console.WriteLine($"방어력 : {player.Defence}");
-            Console.WriteLine($"체 력 : {player.Health}");
-            Console.WriteLine($"Gold : {player.Gold} G");
-            Console.WriteLine("\n0. 나가기\n");
-            Console.WriteLine("원하시는 행동을 입력해주세요.");
-            Console.Write(">> ");
+            while(true)
+            {
+                Console.Clear();
+                Console.WriteLine("상태 보기");
+                Console.WriteLine("캐릭터의 정보가 표시됩니다.\n");
+                Console.WriteLine($"Lv. {Level}");
+                Console.WriteLine($"{Name} ( {ClassName} )");
+                Console.WriteLine($"공격력 : {AttackPower}");
+                Console.WriteLine($"방어력 : {Defence}");
+                Console.WriteLine($"체 력 : {Health}");
+                Console.WriteLine($"Gold : {Gold} G");
+                Console.WriteLine("\n0. 나가기\n");
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                Console.Write(">> ");
 
-            string input = Console.ReadLine();
-            if (input == "0") MenuUI();
+                string input = Console.ReadLine();
+                if (input == "0") MenuUI();
+                else
+                {
+                    Console.WriteLine("잘못된 입력입니다. 계속하려면 아무 키나 누르세요...");
+                    Console.ReadKey();
+                }
+            }
+            
 
         }
         public void InventoryUI()
         {
-            Console.Clear();
-            Console.WriteLine("인벤토리");
-            Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.\n");
-            Console.WriteLine("[아이템 목록]\n");
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("인벤토리");
+                Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.\n");
+                Console.WriteLine("[아이템 목록]\n");
 
-            Console.WriteLine("1. 장착 관리");
-            Console.WriteLine("0. 나가기\n");
-            Console.WriteLine("원하시는 행동을 입력해주세요.");
-            Console.Write(">> ");
+                Console.WriteLine("1. 장착 관리");
+                Console.WriteLine("0. 나가기\n");
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                Console.Write(">> ");
 
-            string input = Console.ReadLine();
-            if (input =="1") ItemManagerUI();
-            else if (input == "0") MenuUI();
+                string input = Console.ReadLine();
+                if (input == "1") ItemManagerUI();
+                else if (input == "0") MenuUI();
+                else
+                {
+                    Console.WriteLine("잘못된 입력입니다. 계속하려면 아무 키나 누르세요...");
+                    Console.ReadKey();
+                }
+            }
+            
         }
 
         public void ItemManagerUI()
         {
-            Console.Clear();
-            Console.WriteLine("인벤토리 - 장착 관리");
-            Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.\n");
-            Console.WriteLine("[아이템 목록]\n");
+            while(true)
+            {
+                Console.Clear();
+                Console.WriteLine("인벤토리 - 장착 관리");
+                Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.\n");
+                Console.WriteLine("[아이템 목록]\n");
 
-            
-            Console.WriteLine("0. 나가기\n");
-            Console.WriteLine("원하시는 행동을 입력해주세요.");
-            Console.Write(">> ");
 
-            string input = Console.ReadLine();
+                Console.WriteLine("0. 나가기\n");
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                Console.Write(">> ");
+
+                string input = Console.ReadLine();
+
+                if (input == "0") InventoryUI();
+                else
+                {
+                    Console.WriteLine("잘못된 입력입니다. 계속하려면 아무 키나 누르세요...");
+                    Console.ReadKey();
+                }
+            }
             
-            if (input == "0") InventoryUI();
         }
 
         public void ShopUI()
         {
-            Console.Clear();
-            Console.WriteLine("상점 - 아이템 판매");
-            Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.\n");
-            Console.WriteLine("[보유 골드]");
-            Console.WriteLine("800 G");
-            Console.WriteLine("[아이템 목록]");
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("상점 - 아이템 판매");
+                Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.\n");
+                Console.WriteLine("[보유 골드]");
+                Console.WriteLine($"{Gold} G");
+                Console.WriteLine("[아이템 목록]");
 
-            Console.WriteLine("\n0. 나가기\n");
-            Console.WriteLine("원하시는 행동을 입력해주세요.");
-            Console.Write(">> ");
+                Console.WriteLine("\n0. 나가기\n");
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                Console.Write(">> ");
 
-            string input = Console.ReadLine();
-            if (input == "0") MenuUI();
+                string input = Console.ReadLine();
+                if (input == "0") MenuUI();
+                else
+                {
+                    Console.WriteLine("잘못된 입력입니다. 계속하려면 아무 키나 누르세요...");
+                    Console.ReadKey();
+                }
+            }
+            
         }
-        // 메인 메서드
-
     }
     class Program
     {
