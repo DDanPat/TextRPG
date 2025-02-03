36e3878c05cbde998d81aa41fc447540ecf7f676
DDanPat <hyoung6855@gmail.com>
2025-02-03 20:15:55 +0900

    플레이어 상속 구조 변경
    게임틀 전부 변경중..
    일단 메인스테이지 클래스와 플레이어 클래스 구조 변경중
    나중에 또 변경 가능성 있음

--- TextRPG/Program.cs
+++ TextRPG/Program.cs
@@ -27,8 +27,10 @@
         public int Level = 1;
         public int Gold = 1500;
 
-        public void ClassSelect(string Select)
+        public Player(string name, string Select)
         {
+            Name = name;
+
             if (Select == "1" || Select == "전사")
             {
                 ClassName = "전사";
@@ -51,76 +53,10 @@
             if (IsDead) Console.WriteLine($"{Name}이(가) 죽었습니다.");
             else Console.WriteLine($"{Name}이(가) {damage}의 데미지를 받았습니다. 남은 체력: {Health}");
         }
-    }
-
-
-    public class MainStage : Player
-    {
-        bool game = true;
-
-        public void Start()
-        {
-            Console.WriteLine("무한의 던전에 오신 여러분 환영합니다.");
-            Console.WriteLine("원하시는 이름을 설정 해주세요.");
-            Console.Write("이름 : ");
-            this.Name = Console.ReadLine();
-
-            //저장 시스템 만들기
-
-            //직업 선택창 만들기
-            Console.Clear();
-            Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
-            Console.WriteLine("원하시는 직업을 골라주세요.");
-            Console.WriteLine("1. 전사 ");
-            Console.WriteLine("2. 도적 ");
-            Console.Write(">> ");
-            string select = Console.ReadLine();
-            this.ClassSelect(select);
-
-            MenuUI();
-        }
-        public void MenuUI()
-        {
-            
-            while (game)
-            {
-                Console.Clear();
-                Console.WriteLine("에데온 마을에 오신 여러분 환영합니다.");
-                Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.\n");
-                Console.WriteLine("1. 상태 보기");
-                Console.WriteLine("2. 인벤토리");
-                Console.WriteLine("3. 상점\n");
-                Console.WriteLine("0. 게임 종료\n");
-                Console.WriteLine("원하시는 행동을 입력해주세요.");
-                Console.Write(">> ");
-                string choice = Console.ReadLine();
-
-                switch (choice)
-                {
-                    case "1":
-                        StateUI();
-                        break;
-                    case "2":
-                        InventoryUI();
-                        break;
-                    case "3":
-                        ShopUI();
-                        break;
-                    case "0":
-                        game = false;
-                        return;
-                    default:
-                        Console.WriteLine("잘못된 입력입니다. 계속하려면 아무 키나 누르세요...");
-                        Console.ReadKey();
-                        break;
-                }
-            }
-            
-        }
 
         public void StateUI()
         {
-            while(game)
+            while (true)
             {
                 Console.Clear();
                 Console.WriteLine("상태 보기");
@@ -137,19 +73,19 @@
 
 
                 string input = Console.ReadLine();
-                if (input == "0") MenuUI();
-                else
-                {
-                    Console.WriteLine("잘못된 입력입니다. 계속하려면 아무 키나 누르세요...");
-                    Console.ReadKey();
-                }
+                //if (input == "0") MainStage.Start();
+                //else
+                //{
+                //    Console.WriteLine("잘못된 입력입니다. 계속하려면 아무 키나 누르세요...");
+                //    Console.ReadKey();
+                //}
             }
-            
 
         }
+
         public void InventoryUI()
         {
-            while (game)
+            while (true)
             {
                 Console.Clear();
                 Console.WriteLine("인벤토리");
@@ -163,19 +99,18 @@
 
                 string input = Console.ReadLine();
                 if (input == "1") ItemManagerUI();
-                else if (input == "0") MenuUI();
-                else
-                {
-                    Console.WriteLine("잘못된 입력입니다. 계속하려면 아무 키나 누르세요...");
-                    Console.ReadKey();
-                }
+                //else if (input == "0") MenuUI();
+                //else
+                //{
+                //    Console.WriteLine("잘못된 입력입니다. 계속하려면 아무 키나 누르세요...");
+                //    Console.ReadKey();
+                //}
             }
-            
-        }
 
+        }
         public void ItemManagerUI()
         {
-            while(game)
+            while (true)
             {
                 Console.Clear();
                 Console.WriteLine("인벤토리 - 장착 관리");
@@ -196,41 +131,109 @@
                     Console.ReadKey();
                 }
             }
-            
+
         }
 
-        public void ShopUI()
+    }
+
+
+    public class MainStage
+    {
+        bool game = true;
+        public void Start(Player player)
         {
             while (game)
             {
                 Console.Clear();
-                Console.WriteLine("상점 - 아이템 판매");
-                Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.\n");
-                Console.WriteLine("[보유 골드]");
-                Console.WriteLine($"{Gold} G");
-                Console.WriteLine("[아이템 목록]");
-
-                Console.WriteLine("\n0. 나가기\n");
+                Console.WriteLine("에데온 마을에 오신 여러분 환영합니다.");
+                Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.\n");
+                Console.WriteLine("1. 상태 보기");
+                Console.WriteLine("2. 인벤토리");
+                Console.WriteLine("3. 상점\n");
+                Console.WriteLine("0. 게임 종료\n");
                 Console.WriteLine("원하시는 행동을 입력해주세요.");
                 Console.Write(">> ");
+                string choice = Console.ReadLine();
 
-                string input = Console.ReadLine();
-                if (input == "0") MenuUI();
-                else
+                switch (choice)
                 {
-                    Console.WriteLine("잘못된 입력입니다. 계속하려면 아무 키나 누르세요...");
-                    Console.ReadKey();
+                    case "1":
+                        player.StateUI();
+                        break;
+                    case "2":
+                        player.InventoryUI();
+                        break;
+                    case "3":
+                        //ShopUI();
+                        break;
+                    case "0":
+                        
+                        return;
+                    default:
+                        Console.WriteLine("잘못된 입력입니다. 계속하려면 아무 키나 누르세요...");
+                        Console.ReadKey();
+                        break;
                 }
             }
-            
         }
+
+
+        
+        
+
+        
+
+        //public void ShopUI()
+        //{
+        //    while (game)
+        //    {
+        //        Console.Clear();
+        //        Console.WriteLine("상점 - 아이템 판매");
+        //        Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.\n");
+        //        Console.WriteLine("[보유 골드]");
+        //        Console.WriteLine($"{player.Gold} G");
+        //        Console.WriteLine("[아이템 목록]");
+
+        //        Console.WriteLine("\n0. 나가기\n");
+        //        Console.WriteLine("원하시는 행동을 입력해주세요.");
+        //        Console.Write(">> ");
+
+        //        string input = Console.ReadLine();
+        //        if (input == "0") MenuUI();
+        //        else
+        //        {
+        //            Console.WriteLine("잘못된 입력입니다. 계속하려면 아무 키나 누르세요...");
+        //            Console.ReadKey();
+        //        }
+        //    }
+            
+        //}
     }
     class Program
     {
         static void Main(string[] args)
         {
+            
+            Console.WriteLine("무한의 던전에 오신 여러분 환영합니다.");
+            Console.WriteLine("원하시는 이름을 설정 해주세요.");
+            Console.Write("이름 : ");
+            string name = Console.ReadLine();
+
+            //저장 시스템 만들기
+
+            //직업 선택창 만들기
+            Console.Clear();
+            Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
+            Console.WriteLine("원하시는 직업을 골라주세요.");
+            Console.WriteLine("1. 전사 ");
+            Console.WriteLine("2. 도적 ");
+            Console.Write(">> ");
+            string select = Console.ReadLine();
+            
+            Player player = new Player(name, select);
+            
             MainStage mainStage = new MainStage();
-            mainStage.Start();
+            mainStage.Start(player);
         }
     }
 }
