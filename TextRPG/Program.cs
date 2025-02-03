a2324b40f3be94bd8c02e8f263c4e3da6e168fef
DDanPat <hyoung6855@gmail.com>
2025-02-03 20:27:17 +0900

    게임 진행 틀? 변경
    전 History에서 변경했던거 다시 MainStage 클레스로 가져와 표시

--- TextRPG/Program.cs
+++ TextRPG/Program.cs
@@ -2,6 +2,8 @@
 {
     using System.Diagnostics;
     using System.Numerics;
+    using System.Reflection.Emit;
+    using System.Xml.Linq;
 
     public interface ICharacter
     {
@@ -54,38 +56,84 @@
             else Console.WriteLine($"{Name}이(가) {damage}의 데미지를 받았습니다. 남은 체력: {Health}");
         }
 
+    }
+
+
+    public class MainStage(Player player)
+    {
+        bool game = true;
+        public void Start()
+        {
+            MainMenu();
+        }
+        public void MainMenu()
+        {
+            while (game)
+            {
+                Console.Clear();
+                Console.WriteLine("에데온 마을에 오신 여러분 환영합니다.");
+                Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.\n");
+                Console.WriteLine("1. 상태 보기");
+                Console.WriteLine("2. 인벤토리");
+                Console.WriteLine("3. 상점\n");
+                Console.WriteLine("0. 게임 종료\n");
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                Console.Write(">> ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        StateUI();
+                        break;
+                    case "2":
+                        InventoryUI();
+                        break;
+                    case "3":
+                        //ShopUI();
+                        break;
+                    case "0":
+                        game = false;
+                        return;
+                    default:
+                        Console.WriteLine("잘못된 입력입니다. 계속하려면 아무 키나 누르세요...");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
         public void StateUI()
         {
-            while (true)
+            while (game)
             {
                 Console.Clear();
                 Console.WriteLine("상태 보기");
                 Console.WriteLine("캐릭터의 정보가 표시됩니다.\n");
-                Console.WriteLine($"Lv. {Level}");
-                Console.WriteLine($"{Name} ( {ClassName} )");
-                Console.WriteLine($"공격력 : {AttackPower}");
-                Console.WriteLine($"방어력 : {Defence}");
-                Console.WriteLine($"체 력 : {Health}");
-                Console.WriteLine($"Gold : {Gold} G");
+                Console.WriteLine($"Lv. {player.Level}");
+                Console.WriteLine($"{player.Name} ( {player.ClassName} )");
+                Console.WriteLine($"공격력 : {player.AttackPower}");
+                Console.WriteLine($"방어력 : {player.Defence}");
+                Console.WriteLine($"체 력 : {player.Health}");
+                Console.WriteLine($"Gold : {player.Gold} G");
                 Console.WriteLine("\n0. 나가기\n");
                 Console.WriteLine("원하시는 행동을 입력해주세요.");
                 Console.Write(">> ");
 
 
                 string input = Console.ReadLine();
-                //if (input == "0") MainStage.Start();
-                //else
-                //{
-                //    Console.WriteLine("잘못된 입력입니다. 계속하려면 아무 키나 누르세요...");
-                //    Console.ReadKey();
-                //}
+                if (input == "0") MainMenu();
+                else
+                {
+                    Console.WriteLine("잘못된 입력입니다. 계속하려면 아무 키나 누르세요...");
+                    Console.ReadKey();
+                }
             }
 
         }
 
         public void InventoryUI()
         {
-            while (true)
+            while (game)
             {
                 Console.Clear();
                 Console.WriteLine("인벤토리");
@@ -99,18 +147,18 @@
 
                 string input = Console.ReadLine();
                 if (input == "1") ItemManagerUI();
-                //else if (input == "0") MenuUI();
-                //else
-                //{
-                //    Console.WriteLine("잘못된 입력입니다. 계속하려면 아무 키나 누르세요...");
-                //    Console.ReadKey();
-                //}
+                else if (input == "0") MainMenu();
+                else
+                {
+                    Console.WriteLine("잘못된 입력입니다. 계속하려면 아무 키나 누르세요...");
+                    Console.ReadKey();
+                }
             }
 
         }
         public void ItemManagerUI()
         {
-            while (true)
+            while (game)
             {
                 Console.Clear();
                 Console.WriteLine("인벤토리 - 장착 관리");
@@ -134,80 +182,37 @@
 
         }
 
-    }
 
 
-    public class MainStage
-    {
-        bool game = true;
-        public void Start(Player player)
+
+
+
+
+        public void ShopUI()
         {
             while (game)
             {
                 Console.Clear();
-                Console.WriteLine("에데온 마을에 오신 여러분 환영합니다.");
-                Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.\n");
-                Console.WriteLine("1. 상태 보기");
-                Console.WriteLine("2. 인벤토리");
-                Console.WriteLine("3. 상점\n");
-                Console.WriteLine("0. 게임 종료\n");
+                Console.WriteLine("상점 - 아이템 판매");
+                Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.\n");
+                Console.WriteLine("[보유 골드]");
+                Console.WriteLine($"{player.Gold} G");
+                Console.WriteLine("[아이템 목록]");
+
+                Console.WriteLine("\n0. 나가기\n");
                 Console.WriteLine("원하시는 행동을 입력해주세요.");
                 Console.Write(">> ");
-                string choice = Console.ReadLine();
 
-                switch (choice)
+                string input = Console.ReadLine();
+                if (input == "0") MainMenu();
+                else
                 {
-                    case "1":
-                        player.StateUI();
-                        break;
-                    case "2":
-                        player.InventoryUI();
-                        break;
-                    case "3":
-                        //ShopUI();
-                        break;
-                    case "0":
-                        
-                        return;
-                    default:
-                        Console.WriteLine("잘못된 입력입니다. 계속하려면 아무 키나 누르세요...");
-                        Console.ReadKey();
-                        break;
+                    Console.WriteLine("잘못된 입력입니다. 계속하려면 아무 키나 누르세요...");
+                    Console.ReadKey();
                 }
             }
+
         }
-
-
-        
-        
-
-        
-
-        //public void ShopUI()
-        //{
-        //    while (game)
-        //    {
-        //        Console.Clear();
-        //        Console.WriteLine("상점 - 아이템 판매");
-        //        Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.\n");
-        //        Console.WriteLine("[보유 골드]");
-        //        Console.WriteLine($"{player.Gold} G");
-        //        Console.WriteLine("[아이템 목록]");
-
-        //        Console.WriteLine("\n0. 나가기\n");
-        //        Console.WriteLine("원하시는 행동을 입력해주세요.");
-        //        Console.Write(">> ");
-
-        //        string input = Console.ReadLine();
-        //        if (input == "0") MenuUI();
-        //        else
-        //        {
-        //            Console.WriteLine("잘못된 입력입니다. 계속하려면 아무 키나 누르세요...");
-        //            Console.ReadKey();
-        //        }
-        //    }
-            
-        //}
     }
     class Program
     {
@@ -232,8 +237,8 @@
             
             Player player = new Player(name, select);
             
-            MainStage mainStage = new MainStage();
-            mainStage.Start(player);
+            MainStage mainStage = new MainStage(player);
+            mainStage.Start();
         }
     }
 }
